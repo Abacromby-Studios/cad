94639b2e2c2a6e8baa61ba74c4acc80e84decb50
Abacromby-Studios <abacromby1739@Gmail.com>
2024-11-20 06:08:15 -0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,28 +1,28 @@
 const config = {}
 
 config.MongoStore = {
-  secret: "yoursecret",
-  url: "mongodb://icehax:tommaso.celano01@ds159344.mlab.com:59344/flecad"
+  secret: "BHEzwIw5tMPwJ8vf",
+  url: "mongodb+srv://UsrMngr-0:<BHEzwIw5tMPwJ8vf>@cluster0.jyq02.mongodb.net/"
 }
 config.text = {
-	sheriff: "here goes the name of your sheriff department",
-	pd: "here goes the name of your police department",
-	ems: "here goes the name of your FIRE RESCUE department",
-	highway: "here goes the name of your highway patrol department",
-	sheriffbackground: "here goes a URL TO THE BACKGROUND IMAGE of your sheriff department",
-	pdbackground: "here goes a URL TO THE BACKGROUND IMAGE of your police department",
-	emsbackground: "here goes a URL TO THE BACKGROUND IMAGE of your fire rescue department",
-	highwaybackground: "here goes a URL TO THE BACKGROUND IMAGE of your highway patrol department",
+	sheriff: "Outagamie Counto Sheriffs Office",
+	pd: "Fox Valley Metropolitan Poloce Department",
+	ems: "Greenvile Fire Department & Outagamie County Emergency Medical Services",
+	highway: "Wisconsin State Patrol",
+	sheriffbackground: "https://cdn.discordapp.com/attachments/1273978731131834549/1308747049218342993/images_1.jpg?ex=673f112b&is=673dbfab&hm=be0dd90fc2160a59c4fe1a68b14ea8fbd45f7d6098ad793f419ae4384faf384d&",
+	pdbackground: "https://cdn.discordapp.com/attachments/1273978731131834549/1308749178327007232/images_3.jpg?ex=673f1327&is=673dc1a7&hm=8151c3967506418ca3700482c69c4f057e7725a0db7279ead051786f8958789b&",
+	emsbackground: "https://cdn.discordapp.com/attachments/1273978731131834549/1308747049604485151/images.jpg?ex=673f112b&is=673dbfab&hm=d791750cc6c94f34af89d0bb8d4c80dd855529057162da154935d16b28e27074&",
+	highwaybackground: "https://cdn.discordapp.com/attachments/1273978731131834549/1308747048983724093/images_2.jpg?ex=673f112b&is=673dbfab&hm=4adb4f386c0d404aafd7ba1d67db54deb8428feaad9cfe9d04f6a0afe7bd533f&",
 	mainbackgroundone: "http://cdn.escapistmagazine.com/media/global/images/library/deriv/632/632377.jpg",
 	mainbackgroundtwo: "http://www.mytrickrc.com/wp-content/uploads/2013/11/Mustang_L34_dark.jpg",
 	mainbackgroundthree: "https://img.gta5-mods.com/q75/images/mastercom-b-siren/02f0b4-AFPGetty-167087672.jpg",
 	maintextone: "Text to be displayed on the first image of the slideshow on the home page",
 	maintexttwo: "Text to be displayed on the second image of the slideshow on the home page",
 	maintextthree: "Text to be displayed on the third image of the slideshow on the home page",
-	sherifflogo: "URL to sheriff logo",
-	pdlogo: "URL to police dept. logo",
-	emslogo: "URL to fire rescue logo",
-	highwaylogo: "URL to highway patrol logo"
+	sherifflogo: "https://cdn.discordapp.com/attachments/1273978731131834549/1308745734765543494/1000000344-removebg-preview.png?ex=673f0ff2&is=673dbe72&hm=59486afdb808e065ba8281ea375f62a40ca9404ce4508ac8538e8d1a5c7f893d&",
+	pdlogo: "https://cdn.discordapp.com/attachments/1273978731131834549/1308745734480465951/1000000335-removebg-preview.png?ex=673f0ff2&is=673dbe72&hm=22079afcb53a7bd0247487190cf4060b09554d7508b3eafd1b9f70bdc759f044&",
+	emslogo: "https://cdn.discordapp.com/attachments/1273978731131834549/1308747822941736981/1000000348-removebg-preview.png?ex=673f11e4&is=673dc064&hm=99fe1d927507f133e116d9e44347b27cbbbc6807302f465c08d8841493021250&",
+	highwaylogo: "https://cdn.discordapp.com/attachments/1273978731131834549/1308745735076053063/dsp-shield-logo.png?ex=673f0ff2&is=673dbe72&hm=8c7ec39943bd9d9522b71e5d318011a1fab675722ba27da4ad2773923031f7c5&"
 
 
 }
